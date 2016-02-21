7c354ac83709c1dd9858c2bfd4dccdbb344f3b30
Tom Sullivan <tom@Toms-MacBook.local>
2016-02-21 15:47:05 +1100

    Add specs for character_spacing.

--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe "PDF::Core::Text" do
   end
 
   let(:mock) { TextMock.new }
+
   describe "horizontal_text_scaling" do
     describe "called without argument" do
       let(:result) { mock.horizontal_text_scaling }
@@ -35,6 +36,31 @@ RSpec.describe "PDF::Core::Text" do
 
       it "outputs correct PDF content" do
         expect(mock.text).to eq("\n110.0 TzTEST\n100.0 Tz")
+      end
+    end
+  end
+
+  describe "character_spacing" do
+    describe "called without argument" do
+      let(:result) { mock.character_spacing }
+
+      it "functions as accessor" do
+        expect(result).to eq(0)
+      end
+    end
+    describe "called with argument" do
+      before do
+        mock.character_spacing(10) do
+          mock.add_content("TEST")
+        end
+      end
+
+      it "resets character_spacing to original value" do
+        expect(mock.character_spacing).to eq(0)
+      end
+
+      it "outputs correct PDF content" do
+        expect(mock.text).to eq("\n10.0 TcTEST\n0.0 Tc")
       end
     end
   end
